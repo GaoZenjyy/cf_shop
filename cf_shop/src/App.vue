eaa91f1554862ffe2f64c87f21a9fcf07773cc15
gaoyuan <gaoyuan@qq.com>
2019-11-18 08:33:53 +0800

    gaoyuan

---
+++ cf_shop/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- 标签栏 -->
+    <van-tabbar :route="true">
+      <van-tabbar-item icon="shop-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="qr-invalid">分类</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-circle-o">我</van-tabbar-item>
+    </van-tabbar>
+    <router-view />
+  </div>
+</template>
+
+
