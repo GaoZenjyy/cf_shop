28bb2d835fcf6cda26bd253e0c9011d3bdc4a769
gaoyuan <1146514841@qq.com>
2019-12-03 16:24:32 +0800

    1

--- cf_shop_api/config.js
+++ cf_shop_api/config.js
@@ -8,7 +8,7 @@ module.exports = {
         port: 3306,
         host: "127.0.0.1",
         user: "root",
-        password: "roor",
+        password: "root",
         database: "cf_shoop",
         multipleStatements:true
     }
